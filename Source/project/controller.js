e5e3546e7564881e01fbcaa0612f07d71aca3f1a
Vasil <magaranov@abv.bg>
2019-03-30 16:54:06 +0200

    expanding folders

--- Source/project/controller.js
+++ Source/project/controller.js
@@ -1,5 +1,7 @@
 var controller = {
+    idCount: 0,
     tree: {},
+    viewMap: {},
     init: function() {
         var model = require('./model');
         this.tree.path = model.projectRoot;
@@ -7,10 +9,43 @@ var controller = {
         if (fs.lstatSync(this.tree.path).isDirectory()) {
             this.tree.isDir = true;
             this.tree.expanded = false;
-        } 
+        }
         else {
             this.tree.isDir = false;
         }
+        this.tree.id = this.idCount++;
+        this.viewMap[this.tree.id] = this.tree;
+    },
+    expand: function(id) {
+        var node = this.viewMap[id];
+        node.expanded = !node.expanded;
+        if (node.children) {
+            var view = require('./view');
+            view.api.refresh();
+            return;
+        }
+        var fs = require('fs');
+        fs.readdir(node.path, function(err, data) {
+            node.children = [];
+            for (var i = 0; i < data.length; ++i) {
+                var p = node.path + '\\' + data[i];
+                var child = {
+                    path: p,
+                    id: controller.idCount++
+                };
+                controller.viewMap[child.id] = child;
+                if (fs.lstatSync(p).isDirectory()) {
+                    child.isDir = true;
+                    child.expanded = false;
+                }
+                else {
+                    child.isDir = false;
+                }
+                node.children.push(child);
+            }
+            var view = require('./view');
+            view.api.refresh();
+        });
     }
 };
 
--- Source/project/view.js
+++ Source/project/view.js
@@ -3,24 +3,33 @@ var viewTemplates = {};
 var view = {
     rootElement: undefined,
     api: {
+        clickEventListener: function(e) {
+            var target = e.target;
+            if (target.getAttribute('directoryExpandButton')) {
+                var id = target.getAttribute('directoryExpandButton');
+                id = parseInt(id);
+                var controller = require('./controller');
+                controller.expand(id);
+                return;
+            }
+        },
         refresh: function() {
             while (view.rootElement.firstChild) {
                 view.rootElement.removeChild(view.rootElement.firstChild);
             }
-
             var ejs = require('ejs');
             var tree = require('./controller').tree;
             var html = ejs.render(viewTemplates['main.ejs'], {tree: tree, api: this});
             view.rootElement.innerHTML = html;
         },
         visualizeFileEntry: function(fe) {
-            console.log(fe);
             var ejs = require('ejs');
             return ejs.render(viewTemplates['fileEntry.ejs'], { fileEntry: fe, api: this });
         }
     },
     init: function(parent) {
         this.rootElement = parent;
+        this.rootElement.addEventListener('click', this.api.clickEventListener);
         var viewFiles = ['main.ejs', 'fileEntry.ejs'];
 
         var fs = require('fs');
