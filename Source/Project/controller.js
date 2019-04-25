c67108a29584072b7acb7e5e53c3a1171ebf9636
Vasil <magaranov@abv.bg>
2019-04-26 01:28:52 +0300

    context menu for project

--- Source/Project/controller.js
+++ Source/Project/controller.js
@@ -1,6 +1,72 @@
+var views = undefined;
+var contextMenuView = 'contextMenu.html';
+var viewsFiles = [contextMenuView];
+var viewsDir = __dirname + '\\Views\\';
 
 var controller = {
+    events: {
+        registered: false,
+        onContextMenu: function (e) {
+            var target = e.target;
+            var fileEntry = target.getAttribute('file-entry');
+            if (!fileEntry)
+                return false;
+
+            fileEntry = parseInt(fileEntry);
+
+            var model = require('./model');
+            fileEntry = model.fileEntries[fileEntry];
+
+            var ejs = require('ejs');
+            var html = ejs.render(views[contextMenuView], { fileEntry: fileEntry });
+
+            var contextPlace = target.querySelector('[context-menu-place]');
+            contextPlace.innerHTML = html;
+            contextPlace.style.left = e.offsetX + 'px';
+            contextPlace.style.top = e.offsetY + 'px';
+
+            var events = require('../events');
+            var clearContextMenu = function (e) {
+                console.log(e);
+                if (e.button !== 0) {
+                    return false;
+                }
+
+                var target = e.target;
+                if (target.getAttribute('project-context-menu')) {
+                    return false;
+                }
+                while (contextPlace.firstChild) {
+                    contextPlace.removeChild(contextPlace.firstChild);
+                }
+                var index = events.eventHandlers.mouseClick.indexOf(clearContextMenu);
+                events.eventHandlers.mouseClick.splice(index, 1);
+                return true;
+            }
+
+            events.eventHandlers.mouseClick.unshift(clearContextMenu);
+
+            return true;
+        },
+        registerEvents: function () {
+            if (controller.events.registered) {
+                return;
+            }
+            controller.events.registered = true;
+            var events = require('../events');
+            events.eventHandlers.contextMenu.push(controller.events.onContextMenu);
+        }
+    },
     create: function () {
+        if (!views) {
+            var utils = require('../utils');
+            utils.readFiles(viewsDir, viewsFiles, function (res) {
+                views = res;
+            });
+        }
+
+        controller.events.registerEvents();
+
         var ctrl = {
             render: function () {
                 var init = require('../init');
@@ -23,8 +89,6 @@ var controller = {
                     wnd.removeChild(wnd.firstChild);
                 }
 
-                console.log('Init Model ferg');
-
                 var model = require('./model');
                 model.init(function () {
                     var root = model.getProjectRoot();
@@ -35,10 +99,6 @@ var controller = {
             }
         };
         return ctrl;
-    },
-    init: function () {
-        var model = require('./model');
-        model.init(function () { });
     }
 };
 
